eb8aea69c1186ad49dedc7621eca34da0b6c57e7
8afyi <information@8a.fyi>
2025-12-18 13:18:35 -0500

    Update psmouseclicker.ps1

--- psmouseclicker.ps1
+++ psmouseclicker.ps1
@@ -1,1 +1,25 @@
-Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class M{[DllImport("user32.dll")] public static extern void mouse_event(int a,int b,int c,int d,int e);}'; while ($true) { [M]::mouse_event(2,0,0,0,0); [M]::mouse_event(4,0,0,0,0); Start-Sleep -Milliseconds 500 }
+if (-not ([System.Management.Automation.PSTypeName]'M').Type) {
+    # Define the interop type once so reruns don't raise TYPE_ALREADY_EXISTS.
+    Add-Type -TypeDefinition @'
+using System;
+using System.Runtime.InteropServices;
+
+public class M
+{
+    [DllImport("user32.dll")]
+    public static extern void mouse_event(int a, int b, int c, int d, int e);
+}
+'@
+}
+
+$count = 0
+
+while ($true) {
+    [M]::mouse_event(2,0,0,0,0)
+    [M]::mouse_event(4,0,0,0,0)
+
+    $count++
+    Write-Host ("Clicks: {0}" -f $count)
+
+    Start-Sleep -Milliseconds 400
+}
